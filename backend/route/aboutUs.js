8b9f9cc3af93642ee11764c3a455a737f0b1d9a8
harshitdhodi21 <harshit.dhodi@rndtechnosoft.in>
2025-01-18 10:38:10 +0530

    changes in product and about us apis

--- backend/route/aboutUs.js
+++ backend/route/aboutUs.js
@@ -3,6 +3,7 @@ const {  createAboutUs,
     getAboutUs,
     updateAboutUs,
     deleteAboutUs,
+    getAboutUsBySlug,
     getAboutUsById,} = require('../controller/aboutUs');
 const image = require('../middleware/imgUpload');
 
@@ -11,5 +12,6 @@ router.get('/get', getAboutUs);
 router.put('/update', image, updateAboutUs);
 router.delete('/delete', deleteAboutUs);
 router.get('/getById', getAboutUsById);
+router.get('/getBySlug', getAboutUsBySlug);
 
 module.exports = router; 
