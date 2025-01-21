5b9a502b5dc0b51d1826e2f088fc56ceb1f0cad0
harshitdhodi21 <harshit.dhodi@rndtechnosoft.in>
2025-01-21 14:28:03 +0530

    done logo

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,13 +10,13 @@ export default defineConfig({
     mimeTypes: {
       "application/javascript": ["js"],
     },             
-    proxy: {
-      "/api": {
-        target: "http://localhost:3028",
-        changeOrigin: true, // Ensures proper handling of CORS
-        secure:false
-          },
-    },
+    // proxy: {
+    //   "/api": {
+    //     target: "http://localhost:3028",
+    //     changeOrigin: true, // Ensures proper handling of CORS
+    //     secure:false
+    //       },
+    // },
   },
   resolve: {
     alias: {
