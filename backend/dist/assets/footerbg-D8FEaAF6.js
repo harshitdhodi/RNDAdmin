4b931fe9c1928eb55d9fb8ac3e01a9b07f345d1e
harshitdhodi21 <harshit.dhodi@rndtechnosoft.in>
2025-03-13 16:40:23 +0530

    new build with no index

---
+++ backend/dist/assets/footerbg-D8FEaAF6.js
@@ -0,0 +1,1 @@
+const e="/assets/footerbg-DFqGxPhu.webp";export{e as default};
--- backend/dist/assets/useForceUpdate-EC55elJS.js
+++
@@ -1,1 +0,0 @@
-import{c as t}from"./vendor-CgeupRFx.js";function c(){const[,e]=t.useReducer(r=>r+1,0);return e}export{c as u};
