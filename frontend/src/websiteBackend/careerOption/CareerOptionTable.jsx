d3360dfc46bfe77ace3e31bb3264c0922c990c4a
Prachibariya <prachibariya03@gmail.com>
2026-01-13 14:14:29 +0530

    updated

--- frontend/src/websiteBackend/careerOption/CareerOptionTable.jsx
+++ frontend/src/websiteBackend/careerOption/CareerOptionTable.jsx
@@ -230,7 +230,7 @@ const CareerOptionTable = () => {
       <div className="flex justify-between items-center mb-4">
         <h1 className="text-xl font-bold  text-gray-700 font-serif uppercase">Career Options</h1>
         <button className="px-4 py-2 bg-slate-700 text-white rounded hover:bg-slate-900 transition duration-300 font-serif">
-          <Link to="/careeroption/createCareerOption"><Plus size={15} /></Link>
+          <Link to="/career/add"><Plus size={15} /></Link>
         </button>
       </div>
       <div className="mb-4">
